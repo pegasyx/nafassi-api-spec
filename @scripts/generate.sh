c6f81eeab9df5b6ea2aacb8d6dbd923007edaa16
Samir <samir_saidali@yahoo.fr>
2023-02-19 16:17:23 +0000

    fix: favicon

--- @scripts/generate.sh
+++ @scripts/generate.sh
@@ -4,4 +4,4 @@ npx redoc-cli build api-spec.json
 mv redoc-static.html index.html
 
 # Might make the favicon work
-# sed -i '7 i \ \ <link rel="icon" type="image/png" href="path_to_favicon"/>' index.html
+sed -i '7 i \ \ <link rel="icon" type="image/png" href="/img/favicon.png"/>' index.html
